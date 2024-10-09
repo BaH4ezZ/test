9679b16244f73f2b03778952b34f4bb0ac4d6a27
BaH4ezZ <bah4ezzmzk@gmail.com>
2024-10-09 18:52:36 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-print("test7")
+string = input("введите номер дня")
+
+dayofweek = str(datetime.datetime.now().weekday())
+if string == "1":
+    print("понедельник")
+elif string =="2":
+    print("вторник")
+elif string =="3":
+    print("среда")
+elif string == "4":
+    print("четверг")
+elif string =="5":
+    print("пятница")
+elif string =="6":
+    print("суббота")
+elif string =="7":
+    print("воскресенье")
+
+else:
+    print("день не соотвецтвует")
