7f04e38fa51fd3e494c0f85a1d645abba019327b
BaH4ezZ <bah4ezzmzk@gmail.com>
2024-10-14 19:33:10 +0700

    Update test.py

--- test.py
+++ test.py
@@ -18,7 +18,66 @@ numbers.sort()
 
 print(numbers)'''
 
-a = int(input("Число 1: "))
+'''a = int(input("Число 1: "))
 b = int(input("Число 2: "))
 if a > b:
-    print(str ("a"))
+    print(str("a"))
+elif a < b:
+    print(str("b"))
+else:
+    print("число неизвестно")'''
+
+
+'''string = input("Введите число от 1 до 100: ")
+print(string)
+if 0 < int(string[0]) > 100:
+    print("Введите число вне диапазона")
+else:
+    number = int(string)
+    # print(number)
+if number % 3 == 0 and number % 5 != 0:  # 17 / 5 -> 3.2, 18 / 3 -> 5.0
+    print("Fizz")
+elif number % 5 == 0 and number % 3 != 0:
+    print("Buzz")
+elif number % 15 == 0:
+    print("FizzBuzz")
+else:
+    print("Число: ",number)
+'''
+
+'''string = input("Введите число и степерь: ").split(" ")
+number = int(string[0])
+stepen - int(string[1])
+
+if 0 <= stepen <= 7:
+    result = number ** stepen
+    print("Результат: ", result)'''
+
+string = input("ВВедите стоимость, с кого звоним, куда звоним: ").split(" ")
+price = float(string[0])
+# mtot = 0 #  с мтс на теле 0.2 m
+# mtob = 1 # с мтс на билайн 0.3 b
+# ttob = 2 # c теле на билайн 0.4 t
+# mtom = 3 # мтс 0.1
+# btob = 4 # билайн 0.1
+# ttot = 5 # теле 0.1
+if string[1] =="m" and string[2] =="t":
+    result = price * 0.2
+    print(result)
+elif string[1] == "m" and string[2] == "b":
+        result = price * 0.3
+        print(result)
+elif string[1] == "t" and string[2] == "b":
+        result = price * 0.4
+        print(result)
+elif string[1] == "m" and string[2] == "m":
+        result = price * 0.1
+        print(result)
+elif string[1] == "b" and string[2] == "b":
+        result = price * 0.1
+        print(result)
+elif string[1] == "t" and string[2] == "t":
+        result = price * 0.1
+        print(result)
+
+
