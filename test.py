5814664019ed12f70008c3d1eff8e21a84085a7b
BaH4ezZ <bah4ezzmzk@gmail.com>
2024-10-16 20:39:39 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,23 @@
-string = input("Enter: ").split(" ")
+'''string = input("Enter: ").split(" ")
 begin = int(string[0])
-end = int(string[1])
+end = int(string[1])'''
 
 '''result = ""
 for i in range(begin, end + 1):
         result += str(i) + " "
 print( result)'''
 
-result = ""
+'''result = ""
 j = end
 for i in range(begin, end + 1):
     result += str(j) + " "
     j -= 1
-print( result)
+print( result)'''
+
+'''count = 0
+for i in range(begin, end+1):
+    if i % 5 ==0 and i != 0:
+        count+=1
+print(count)'''
+
+
